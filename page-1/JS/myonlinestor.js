5938884342b3c025029ec4ed8cf1aaac32a53753
Sanskargoyal608 <sanskargoyal608@gmail.com>
2023-10-15 13:13:12 +0530

    online store

---
+++ page-1/JS/myonlinestor.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+document.querySelector(".vector-67K").addEventListener("click", function() {
+    var hiddenBox = document.querySelector(".auto-group-8cuw-hgV");
+    if (hiddenBox.style.display === "none") {
+      hiddenBox.style.display = "block";
+      console.log("it is running");
+    } else {
+      hiddenBox.style.display = "none";
+      console.log("it is not running");
+    }
+  });
+  document.querySelector(".vector-gxR").addEventListener("click", function() {
+    var hiddenBox = document.querySelector(".group-116-nkq");
+    if (hiddenBox.style.display === "none") {
+      hiddenBox.style.display = "block";
+      console.log("it is running");
+    } else {
+      hiddenBox.style.display = "none";
+      console.log("it is not running");
+    }
+  });
+
+
+});
